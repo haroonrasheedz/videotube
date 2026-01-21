04ef6d3ac25de89d5e65112ae6b9e0e78e084642
haroonrasheedz <97180643+haroonrasheedz@users.noreply.github.com>
2026-01-21 11:38:21 +0530

    Add files via upload

---
+++ scripts/data.js
@@ -0,0 +1,99 @@
+export let videoData;
+
+videoData = [{
+  videoLink: 'https://www.youtube.com/watch?v=iR5nd2xiAL4',
+  thumbnail: 'thumbnails/thumbnail-1.webp',
+  videoDuration: '9:55',
+  profilePic: 'Profile-pics/channel-1.jpg',
+  videoTitle: 'THE QUALIFIER 🏆 Perth Scorchers v Sydney Sixers Match Highlights',
+  videoAuthor: 'The Big Bash',
+  videoStats: '1.4M views · 4 hours ago'
+},{
+  videoLink: 'https://www.youtube.com/watch?v=L-hLVS_53NU',
+  thumbnail: 'thumbnails/thumbnail-2.webp',
+  videoDuration: '46:38',
+  profilePic: 'Profile-pics/channel-2.jpg',
+  videoTitle: 'GMOs Jeremy Grantham Sees an AI Bubble — and a Familiar Ending',
+  videoAuthor: 'Bloomberg Podcasts',
+  videoStats: '18K views · 1 day ago'
+},{
+  videoLink: 'https://www.youtube.com/watch?v=QLOwG35Eows',
+  thumbnail: 'thumbnails/thumbnail-3.webp',
+  videoDuration: '21:15',
+  profilePic: 'Profile-pics/channel-3.jpg',
+  videoTitle: 'NVIDIA’s $4 Trillion AI Bubble',
+  videoAuthor: 'Peruvian Bull',
+  videoStats: '6.4K views · 5 days ago'
+},{
+  videoLink: 'https://www.youtube.com/watch?v=8pBk4IUEMOk',
+  thumbnail: 'thumbnails/thumbnail-4.webp',
+  videoDuration: '32:29',
+  profilePic: 'Profile-pics/channel-4.jpg',
+  videoTitle: 'The Best Battles Of The 2025 Season!',
+  videoAuthor: 'Veritasium',
+  videoStats: '434K views · 2 weeks ago'
+},{
+  videoLink: 'https://www.youtube.com/watch?v=CiqytGuaoEs',
+  thumbnail: 'thumbnails/thumbnail-5.webp',
+  videoDuration: '09:17',
+  profilePic: 'Profile-pics/channel-5.jpg',
+  videoTitle: 'Chelsea 2-3 Arsenal | HIGHLIGHTS - Extended | Carabao Cup 2025/26',
+  videoAuthor: 'Chelsea',
+  videoStats: '2.1M views · 5 days ago'
+},{
+  videoLink: 'https://www.youtube.com/watch?v=fOH7_nqjEv8',
+  thumbnail: 'thumbnails/thumbnail-6.webp',
+  videoDuration: '20:52',
+  profilePic: 'Profile-pics/channel-6.jpg',
+  videoTitle: 'The Biggest Monsters of Season 2 | River Monsters | Animal Planet',
+  videoAuthor: 'Animal Planet',
+  videoStats: '1.2M views · 1 year ago'
+},{
+  videoLink: 'https://www.youtube.com/watch?v=MfRzxFhqLLo',
+  thumbnail: 'thumbnails/thumbnail-7.webp',
+  videoDuration: '1:05:33',
+  profilePic: 'Profile-pics/channel-7.jpg',
+  videoTitle: 'Surviving in Deadly Deserts | Man Vs. Wild | Discovery',
+  videoAuthor: 'Discovery',
+  videoStats: '533K views · 3 months ago'
+},{
+  videoLink: 'https://www.youtube.com/watch?v=fluXf5lYiM4',
+  thumbnail: 'thumbnails/thumbnail-8.webp',
+  videoDuration: '35:14',
+  profilePic: 'Profile-pics/channel-8.jpg',
+  videoTitle: 'Della Maddalena vs Islam Makhachev | Highlights | UFC 322 | Welterweight Title | 16th November 2025',
+  videoAuthor: 'Sony LIV',
+  videoStats: '410K views · 1 month ago'
+},{
+  videoLink: 'https://www.youtube.com/watch?v=qmHkLU4OMmE',
+  thumbnail: 'thumbnails/thumbnail-9.webp',
+  videoDuration: '1:35:33',
+  profilePic: 'Profile-pics/channel-9.jpg',
+  videoTitle: 'NYC Mayor Mamdanis official swearing-in ceremony',
+  videoAuthor: 'LiveNOW from FOX',
+  videoStats: '12K views · 2 weeks ago'
+},{
+  videoLink: 'https://www.youtube.com/watch?v=kY8UxDMJtpM&list=RDkY8UxDMJtpM&start_radio=1',
+  thumbnail: 'thumbnails/thumbnail-10.webp',
+  videoDuration: '27:16',
+  profilePic: 'Profile-pics/channel-10.jpg',
+  videoTitle: 'FiFa World Cup Qatar 2022 Opening Ceremony Full Show | HD',
+  videoAuthor: 'Saims Music',
+  videoStats: '884K views · 3 years ago'
+},{
+  videoLink: 'https://www.youtube.com/watch?v=kk60WsqwoRw',
+  thumbnail: 'thumbnails/thumbnail-11.webp',
+  videoDuration: '44:28',
+  profilePic: 'Profile-pics/channel-11.jpg',
+  videoTitle: 'Mamba in My Closet! (Full Episode) | Snake City',
+  videoAuthor: 'Nat Geo Animals',
+  videoStats: '2.3M views · 5 years ago'
+},{
+  videoLink: 'https://www.youtube.com/watch?v=dDd9vJwz2-I',
+  thumbnail: 'thumbnails/thumbnail-12.webp',
+  videoDuration: '36:09',
+  profilePic: 'Profile-pics/channel-12.jpg',
+  videoTitle: 'A.I Is The Biggest Lie Ever Told. Heres Why.',
+  videoAuthor: 'Moon',
+  videoStats: '487K views · 2 month ago'
+}];
